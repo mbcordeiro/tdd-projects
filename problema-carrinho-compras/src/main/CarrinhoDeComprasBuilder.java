89c55782c00fbb740a022877a60dccb320ec0b84
mbcordeiro <matheusdebarroscordeiro@gmail.com>
2021-10-13 13:38:11 -0300

    Refactor tests

---
+++ problema-carrinho-compras/src/main/CarrinhoDeComprasBuilder.java
@@ -0,0 +1,20 @@
+package main;
+
+public class CarrinhoDeComprasBuilder {
+    public CarrinhoDeCompras carrinho;
+
+    public CarrinhoDeComprasBuilder() {
+        this.carrinho = new CarrinhoDeCompras();
+    }
+
+    public CarrinhoDeComprasBuilder comItens(double... valores) {
+        for(double valor : valores) {
+            carrinho.adiciona(new Item("item", 1, valor));
+        }
+        return this;
+    }
+
+    public CarrinhoDeCompras cria() {
+        return carrinho;
+    }
+}
--- problema-carrinho-compras/src/test/CarrinhoDeComprasTest.java
+++ problema-carrinho-compras/src/test/CarrinhoDeComprasTest.java
@@ -2,25 +2,30 @@ package test;
 
 import main.CarrinhoDeCompras;
 import main.Item;
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
 public class CarrinhoDeComprasTest {
+    private CarrinhoDeCompras carrinho;
+
+    @Before
+    public void inicializa() {
+        this.carrinho = new CarrinhoDeCompras();
+    }
+
     @Test
     public void deveRetornarZeroSeCarrinhoVazio() {
-        CarrinhoDeCompras carrinho = new CarrinhoDeCompras();
         assertEquals(0.0, carrinho.maiorValor(), 0.0001);
     }
     @Test
     public void deveRetornarValorDoItemSeCarrinhoCom1Elemento() {
-        CarrinhoDeCompras carrinho = new CarrinhoDeCompras();
         carrinho.adiciona(new Item("Geladeira", 1, 900.0));
         assertEquals(900.0, carrinho.maiorValor(), 0.0001);
     }
     @Test
     public void deveRetornarMaiorValorSeCarrinhoContemMuitosElementos() {
-        CarrinhoDeCompras carrinho = new CarrinhoDeCompras();
         carrinho.adiciona(new Item("Geladeira", 1, 900.0));
         carrinho.adiciona(new Item("Fogão", 1, 1500.0));
         carrinho.adiciona(new Item("Máquina de Lavar", 1, 750.0));
