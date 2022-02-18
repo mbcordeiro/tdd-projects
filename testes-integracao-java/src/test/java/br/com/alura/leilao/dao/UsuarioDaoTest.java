eaa7de519dbe6d9c08be137ac5b4bca501de4a66
mbcordeiro <matheusdebarroscordeiro@gmail.com>
2022-02-18 00:58:28 -0300

    Config tests

--- testes-integracao-java/src/test/java/br/com/alura/leilao/dao/UsuarioDaoTest.java
+++ testes-integracao-java/src/test/java/br/com/alura/leilao/dao/UsuarioDaoTest.java
@@ -1,5 +1,7 @@
 package br.com.alura.leilao.dao;
 
+import br.com.alura.leilao.model.Usuario;
+import br.com.alura.leilao.util.JPAUtil;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -8,11 +10,17 @@ import javax.persistence.EntityManager;
 class UsuarioDaoTest {
     private UsuarioDao usuarioDao;
 
-    private EntityManager em;
-
     @Test
-    void testeBuscaUsuarioPeloUsername() {
+    void testeBuscaUsuarioPorUsername() {
+        final EntityManager em = JPAUtil.getEntityManager();
         this.usuarioDao = new UsuarioDao(em);
-        Assertions.assertNotNull(usuarioDao);
+
+        final Usuario usuario = new Usuario("fulano", "fulano@email.com","12345678");
+        em.getTransaction().begin();
+        em.persist(usuario);
+        em.getTransaction().commit();
+
+        final Usuario usuarioEncontrado = this.usuarioDao.buscarPorUsername(usuario.getNome());
+        Assertions.assertNotNull(usuario);
     }
 }
