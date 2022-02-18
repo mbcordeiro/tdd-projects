87d577183793bb2f914e4bb36fb1730224ab0fdf
mbcordeiro <matheusdebarroscordeiro@gmail.com>
2022-02-18 00:38:36 -0300

    Inject dependency entity manager into constructor

--- testes-integracao-java/src/test/java/br/com/alura/leilao/dao/UsuarioDaoTest.java
+++ testes-integracao-java/src/test/java/br/com/alura/leilao/dao/UsuarioDaoTest.java
@@ -3,12 +3,16 @@ package br.com.alura.leilao.dao;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import javax.persistence.EntityManager;
+
 class UsuarioDaoTest {
     private UsuarioDao usuarioDao;
 
+    private EntityManager em;
+
     @Test
     void testeBuscaUsuarioPeloUsername() {
-        this.usuarioDao = new UsuarioDao();
+        this.usuarioDao = new UsuarioDao(em);
         Assertions.assertNotNull(usuarioDao);
     }
 }
